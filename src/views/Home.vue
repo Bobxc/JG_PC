deb80cfc1a536e2bf9a748fabf4e1b299216d2d5
Bobxc <987257718@qq.com>
2022-07-03 19:48:24 +0800

    fix(main): main add hello

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <div class="test">test</div>
+    <div class="hello">hello</div>
   </div>
 </template>
 
